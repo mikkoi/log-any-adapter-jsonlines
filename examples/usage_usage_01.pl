30ebf315cf96b71c2991d66e7b634db8c3437134
Mikko Koivunalho <mikkoi@cpan.org>
2026-01-20 15:33:38 +0100

    Add examples

---
+++ examples/usage_usage_01.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.008_006;
+our $VERSION = 0.001;
+
+use Log::Any qw( $log );
+use Log::Any::Adapter( 'JSONLines',
+    canonical => 1,
+);
+
+# ###################################################################
+# main
+sub main {
+    use Log::Any qw( $log );
+
+    # $log->debug('Create account', { nr=>'12345', user=>'Smith'});
+
+    # $log->context->{user} = 'Smith';
+    # $log->debug('Create account', { nr=>'12345' });
+
+    # $log->context->{user} = 'Smith';
+    # $log->context->{nr} = '12345';
+    # $log->debug('Create account');
+
+    # $log->debug('Create account');
+
+    # $log->debug('Create account', {});
+
+    # $log->debug('Create account', 'New Account', { nr=>'12345'}, {user=>'Smith'});
+
+    # $log->debugf('Create account: %s', '12345', {user=>'Smith'});
+
+    $log->context->{user} = 'Smith';
+    $log->context->{nr} = '12345';
+    $log->debugf('Create account: %s', '12345', {user=>'Smith'});
+    return 0;
+}
+
+exit main(@ARGV);
